f32c990b5b0c04667b0c44436b56d1efa5829583
Juhaied <juhaied.hossen@northsouth.edu>
2023-08-22 16:13:16 +0600

    Click Events

--- blogapp/src/Home.js
+++ blogapp/src/Home.js
@@ -1,7 +1,25 @@
 const Home = () => {
+  const handleClick = () =>{
+    console.log('CLicked');
+  }
+ 
   return (
     <div className="home">
-        <h2>Homepage</h2>      
+        <h2>Homepage</h2>
+        <button onClick={handleClick}> {/* This is function call without parameters*/}
+            Click
+        </button>
+
+        {/*
+
+         const handleClickAgian = (name) =>{
+                    console.log('handle click agained by '+name);
+                }
+
+        <button onClick={() => {handleClickAgian('nabid')}}> {/* This is function call with parameters*
+           Click
+        </button>   
+        */}
     </div>
   )
 }
