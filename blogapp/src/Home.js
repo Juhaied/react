183f8a6f4b73309da4ebb2e9ee9e42187d189df1
Juhaied <juhaied.hossen@northsouth.edu>
2023-08-22 18:55:40 +0600

    Fetching Data with Use Effect form JSON

--- blogapp/src/Home.js
+++ blogapp/src/Home.js
@@ -3,25 +3,22 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   
-  const [blogs,setBlogs] = useState([
-    {title: 'Java website for the students',body:'Java course',author:'Juhaied',id:1},
-    {title: 'C++ website',body:'Intro to C++ course',author:'Toufique',id:2},
-    {title: 'Dot net specialist',body:'From beginner to pro level',author:'Rayhan',id:3}
-  ]);
+  const [blogs,setBlogs] = useState(null);
 
-
-  const handleDelete = (id) =>{
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
 
   useEffect(()=>{
-    console.log();
+    fetch('http://localhost:8000/blogs').
+    then(res =>{
+        return res.json();
+    })
+    .then((data)=>{
+        setBlogs(data);
+    })
   },[]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   )
 }
