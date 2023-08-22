9e7377cffe23cfa7174b58630acedf5709c5f904
Juhaied <juhaied.hossen@northsouth.edu>
2023-08-22 16:21:43 +0600

    Using state

--- blogapp/src/Home.js
+++ blogapp/src/Home.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
-  const handleClick = () =>{
-    console.log('CLicked');
-  }
+
+    const[name,setName] = useState('mario');
+    const handleClick = () =>{
+        setName('Juhaied');
+    }
  
   return (
     <div className="home">
         <h2>Homepage</h2>
+        <p>{name}</p>
         <button onClick={handleClick}> {/* This is function call without parameters*/}
             Click
         </button>
